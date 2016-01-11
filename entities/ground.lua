fd571a8f17c89aeeb9848e9f75434aa99c589f82
caiges <caige@fastmail.fm>
2016-01-10 19:26:01 -0700

    Remove logging

--- entities/ground.lua
+++ entities/ground.lua
@@ -9,15 +9,6 @@ function Ground:new(world)
   body = love.physics.newBody(world, love.window.getWidth() / 2, 500, 'static')
   shape = love.physics.newRectangleShape(width, height)
   fixture = love.physics.newFixture(body, shape)
-  x1, y1, x2, y2, x3, y3, x4, y4 = body:getWorldPoints(shape:getPoints())
-  trace.print(x1, trace.styles.yellow)
-  trace.print(y1, trace.styles.yellow)
-  trace.print(x2, trace.styles.yellow)
-  trace.print(y2, trace.styles.yellow)
-  trace.print(x3, trace.styles.yellow)
-  trace.print(y3, trace.styles.yellow)
-  trace.print(x4, trace.styles.yellow)
-  trace.print(y4, trace.styles.yellow)
 
   newObj = {
     x = x,
